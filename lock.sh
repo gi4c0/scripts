930c712720f3eef376c27012b2f7be3fa1da5757
alexpan <panchenko.aleksey.a@gmail.com>
2020-12-02 22:17:16 +0200

    Add comments

--- lock.sh
+++ lock.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# Script takes a screenshot by scrot, makes it unreadable and put provided 
+# as argument image to center. Then locks screen with this image
+
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 i3lock -u -i /tmp/screen.png
 rm /tmp/screen.png
